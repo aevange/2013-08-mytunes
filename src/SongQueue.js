781231cd890401dad81e98b95bdc0434b9b09aef
Hack Reactor Students <github@hackreactor.com>
2013-08-22 16:20:33 -0700

    Code Clean up for SongQueue.js

--- src/SongQueue.js
+++ src/SongQueue.js
@@ -3,16 +3,12 @@ var SongQueue = Songs.extend({
   initialize: function(){
     this.on('add', function(song){
       this.add(song);
-      if (this.length === 1){
-        this.playFirst();
-      }
+      this.length === 1 && this.playFirst();
     }, this);
 
     this.on('ended', function(){
       this.shift();
-      if(this.length > 0) {
-        this.playFirst();
-      }
+      this.length && this.playFirst();
     }, this);
 
     this.on('dequeue', function(song) {
