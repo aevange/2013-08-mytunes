f8c20f82097939d4081d6e24e8c63fc24b10fc90
aevange <andre.evangelesta@gmail.com>
2013-09-03 08:43:24 -0700

    Add the rest of the mp3s to the data.js file

--- src/data.js
+++ src/data.js
@@ -18,5 +18,46 @@ var songData = [
     url: "mp3s/04 One In A Million.mp3",
     title: "One In A Million",
     artist: "Aaliyah"
-  }
+  },
+
+  {
+    url: "mp3s/03 Age Ain't Nothing But A Number.mp3",
+    title: "Age Ain't Nothing But A Number.mp3",
+    artist: "Aaliyah"
+  },
+  {
+    url: "mp3s/06 If Your Girl Only Knew.mp3",
+    title: "If Your Girl Only Knew",
+    artist: "Aaliyah"
+  },
+  {
+    url: "mp3s/07 Choosey Lover [Old School_New School].mp3",
+    title: "Choosey Lover [Old School_New School]",
+    artist: "Aaliyah"
+  },
+  {
+    url: "mp3s/09 Everything's Gonna Be Alright.mp3",
+    title: "Everything's Gonna Be Alright",
+    artist: "Aaliyah"
+  },
+  {
+    url: "mp3s/12 Rock The Boat.mp3",
+    title: "Rock The Boat",
+    artist: "Aaliyah"
+  },
+   {
+    url: "mp3s/14 More Than A Woman.mp3",
+    title: "More Than A Woman",
+    artist: "Aaliyah"
+  }, 
+  {
+    url: "mp3s/15 Read Between The Lines.mp3",
+    title: "Read Between The Lines",
+    artist: "Aaliyah"
+  },
+   {
+    url: "mp3s/16 It's Whatever.mp3",
+    title: "It's Whatever",
+    artist: "Aaliyah"
+  },
 ];
